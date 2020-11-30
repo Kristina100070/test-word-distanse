a68cf057fca964dbbcc8a014006edef5c60c8c3e
Kristina Gromova <christina28romova@yandex.ru>
2020-11-30 09:48:12 +0300

    work

--- index.js
+++ index.js
@@ -2,59 +2,36 @@ const a = document.querySelector('.word1');
 const b = document.querySelector('.word2');
 const string = document.querySelector('.string');
 
-function countMinDistance(str, wordA, wordB) {
+function countMinDistance(str, wordA, wordB, e) {
+    ;
     let wordAIndex = null;
     let wordBIndex = null;
     let words = str.split(" ");
     let length = words.length;
     for (var i = 0; i < length; i++) {
-      
+     
       if (words[i] === wordA) {
         wordAIndex = words.indexOf(words[i]);
       }
   
-      if (words[i] === wordB) {
-        wordBIndex = words.indexOf(words[i]);
-      }
-  
+      if (words[i] === wordB && e.target.classList.contains("min-count")) {
+           wordBIndex = words.indexOf(words[i]);
+           console.log(wordBIndex);
+        }
+      if (words[i] === wordB && e.target.classList.contains("max-count")) {
+           wordBIndex = words.lastIndexOf(words[i]);
+       }
+      
       if (wordAIndex !== null && wordBIndex !== null) {
         let distance = Math.abs(wordAIndex - wordBIndex) - 1;
-        document.querySelector('.result1').textContent = `Минимальная дистанция  ${distance} `;
+        document.querySelector('.result').textContent = `Минимальная дистанция  ${distance} `;
         return distance;
       }
     }
 }
 
-document.querySelector('.min-count').addEventListener('click', (event) => {
+document.querySelector('.button').addEventListener('click', (event) => {
 event.preventDefault;
-countMinDistance(string.value, a.value, b.value)
+countMinDistance(string.value, a.value, b.value, event)
 });
 
-function countMaxDistance(str, wordA, wordB) {
-    let wordAIndex = null;
-    let wordBIndex = null;
-    let words = str.split(" ");
-    let length = words.length;
-    for (var i = 0; i < length; i++) {
-      
-      if (words[i] === wordA) {
-        wordAIndex = words.indexOf(words[i]);
-      }
-  
-      if (words[i] === wordB) {
-        wordBIndex = words.lastIndexOf(words[i]);
-      }
-  
-      if (wordAIndex !== null && wordBIndex !== null) {
-        let distance = Math.abs(wordAIndex - wordBIndex) - 1;
-        document.querySelector('.result2').textContent = `Максимальная дистанция  ${distance}`;
-        return distance;
-      }
-      
-    }
-}
-
-document.querySelector('.max-count').addEventListener('click', (event) => {
-event.preventDefault;
-countMaxDistance(string.value, a.value, b.value)
-});
