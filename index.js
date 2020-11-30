d78a34e17cf52fe4d9d94a0bcc1eb04a7fc7103a
Kristina Gromova <christina28romova@yandex.ru>
2020-11-30 19:22:46 +0300

    work

--- index.js
+++ index.js
@@ -2,36 +2,33 @@ const a = document.querySelector('.word1');
 const b = document.querySelector('.word2');
 const string = document.querySelector('.string');
 
-function countMinDistance(str, wordA, wordB, e) {
-    ;
+function countDistance(str, wordA, wordB, e) {
+    
     let wordAIndex = null;
     let wordBIndex = null;
     let words = str.split(" ");
-    let length = words.length;
-    for (var i = 0; i < length; i++) {
-     
-      if (words[i] === wordA) {
-        wordAIndex = words.indexOf(words[i]);
+
+    words.forEach(function(item, i, arr) {
+      if (item === wordA) {
+        wordAIndex = words.indexOf(arr[i]);
+        console.log(wordAIndex);
       }
-  
-      if (words[i] === wordB && e.target.classList.contains("min-count")) {
-           wordBIndex = words.indexOf(words[i]);
-           console.log(wordBIndex);
-        }
-      if (words[i] === wordB && e.target.classList.contains("max-count")) {
-           wordBIndex = words.lastIndexOf(words[i]);
+      if (item === wordB && e.target.classList.contains("min-count")) {
+        wordBIndex = words.indexOf(arr[i]);
+          console.log(wordBIndex);
        }
-      
-      if (wordAIndex !== null && wordBIndex !== null) {
-        let distance = Math.abs(wordAIndex - wordBIndex) - 1;
-        document.querySelector('.result').textContent = `Минимальная дистанция  ${distance} `;
-        return distance;
-      }
+       if (item === wordB && e.target.classList.contains("max-count")) {
+        wordBIndex = words.lastIndexOf(words[i]);
+        console.log(wordBIndex);
     }
-}
+    if (wordAIndex !== null && wordBIndex !== null) {
+      let distance = Math.abs(wordAIndex - wordBIndex) - 1;
+      document.querySelector('.result').textContent = `Дистанция  ${distance} `;
+    }
+    });
+  } 
 
 document.querySelector('.button').addEventListener('click', (event) => {
 event.preventDefault;
-countMinDistance(string.value, a.value, b.value, event)
+countDistance(string.value, a.value, b.value, event)
 });
-
